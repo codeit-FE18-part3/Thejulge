71f8c056dc8cdbd68d2d207478e8863d41e32793
celine <wisohyun0@gmail.com>
2025-09-29 21:33:49 +0900

    ♻️ refactor: container 컴포넌트 grow 명시적으로 변경  #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Wrapper>
         <Header />
-        <Container>
+        <Container grow={true}>
           <Component {...pageProps} />
         </Container>
       </Wrapper>
