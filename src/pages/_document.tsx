77e07bfd63113a4f87453081c3aaee2659a2e730
celine <wisohyun0@gmail.com>
2025-09-29 15:16:20 +0900

    🔧 chore: 공통 title 작성 및 , favicon 위치 수정 #1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head>
-        <link rel='icon' href='/favicon.ico' sizes='any' />
-        <link rel='icon' href='/favicon.png' type='image/png' sizes='192x192' />
-      </Head>
+      <Head />
       <body className='antialiased'>
         <Main />
         <NextScript />
