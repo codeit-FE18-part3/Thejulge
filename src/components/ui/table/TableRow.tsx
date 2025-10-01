0f5152eca3527ef5757b9072ee1cf699955de358
gummmmmy0v0 <ppottatto524@gmail.com>
2025-10-01 17:00:46 +0900

    ♻️ refactor: TableRow 변경된 getTime 적용 #6

--- src/components/ui/table/TableRow.tsx
+++ src/components/ui/table/TableRow.tsx
@@ -10,14 +10,14 @@ const TD_BASE = 'border-b px-2 py3';
 const TD_STATUS = 'border-b px-2 py-[9px]';
 
 export default function TableRow({ rowData, variant }: TableTypeVariant) {
+  const { STRAT, END, duration } = getTime(rowData.startsAt, rowData.workhour);
+
   return (
     <tr className='text-left'>
       <td className={TD_BASE}>{rowData.name}</td>
       {variant === 'employee' ? (
         <>
-          <td className={TD_BASE}>
-            {`${getTime(rowData.startsAt, 0)} ~ ${getTime(rowData.startsAt, rowData.workhour)} (${rowData.workhour}시간)`}
-          </td>
+          <td className={TD_BASE}>{`${STRAT} ~ ${END} (${duration})`}</td>
           <td className={TD_BASE}>{rowData.hourlyPay}</td>
         </>
       ) : (
