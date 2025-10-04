4290ecfb4a60db6e490f08fe6cd89f0017457949
gummmmmy0v0 <ppottatto524@gmail.com>
2025-10-04 12:26:23 +0900

    ♻️ refactor: alert 중첩 구조 분해 할당 #20

--- src/components/ui/modal/notification/NotificationMessage.tsx
+++ src/components/ui/modal/notification/NotificationMessage.tsx
@@ -11,9 +11,21 @@ export default function NotificationMessage({
   alert: Alert;
   onRead: (id: string) => void;
 }) {
-  const RESULT_TEXT = alert.result === 'accepted' ? '승인' : '거절';
+  const {
+    id,
+    result,
+    read,
+    createdAt,
+    shop: {
+      item: { name: shopName },
+    },
+    notice: {
+      item: { startsAt, workhour },
+    },
+  } = alert;
+  const RESULT_TEXT = result === 'accepted' ? '승인' : '거절';
 
-  const DATE_RANGE = getTime(alert.notice.item.startsAt, alert.notice.item.workhour);
+  const DATE_RANGE = getTime(startsAt, workhour);
 
   const NOTIFICATION_MESSAGE_CONTAINER = clsx(
     'w-full gap-2 break-words rounded border border-gray-200 bg-white px-3 py-4'
@@ -21,28 +33,28 @@ export default function NotificationMessage({
 
   return (
     <div className={NOTIFICATION_MESSAGE_CONTAINER}>
-      <button onClick={() => onRead(alert.id)} className='w-full text-left'>
+      <button onClick={() => onRead(id)} className='w-full text-left'>
         <div className='flex flex-col gap-2'>
-          <ResultBadge result={alert.result} />
+          <ResultBadge result={result} />
           <p
             className={clsx('text-sm', {
-              'text-gray-400': alert.read,
+              'text-gray-400': read,
             })}
           >
-            {`${alert.shop.item.name} (${DATE_RANGE.date} ${DATE_RANGE.startTime} ~ 
+            {`${shopName} (${DATE_RANGE.date} ${DATE_RANGE.startTime} ~ 
             ${DATE_RANGE.endTime}) 공고 지원이 `}
             <span
               className={clsx({
-                'text-gray-500': alert.read,
-                'text-blue-200': !alert.read && alert.result === 'accepted',
-                'text-red-400': !alert.read && alert.result === 'rejected',
+                'text-gray-500': read,
+                'text-blue-200': !read && result === 'accepted',
+                'text-red-400': !read && result === 'rejected',
               })}
             >
               {RESULT_TEXT}
             </span>
             되었어요.
           </p>
-          <span className='text-xs text-gray-400'>{timeAgo(alert.createdAt)}</span>
+          <span className='text-xs text-gray-400'>{timeAgo(createdAt)}</span>
         </div>
       </button>
     </div>
