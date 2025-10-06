40201102ec9144cb6ec9a715ca81b50aa12910a0
celine <wisohyun0@gmail.com>
2025-10-07 02:02:13 +0900

    ✨ feat: 로고 size 추가 및 Nav 권한별 UI 구현 #30

--- src/components/layout/header/nav.tsx
+++ src/components/layout/header/nav.tsx
@@ -1,15 +1,44 @@
 import { Icon } from '@/components/ui';
+import useAuth from '@/hooks/useAuth';
 import { cn } from '@/lib/utils/cn';
+import { UserRole } from '@/types/user';
 import Link from 'next/link';
+import { ReactNode } from 'react';
+
+const NAV_CONFIG: Record<UserRole, ReactNode> = {
+  guest: (
+    <>
+      <Link href='/login'>로그인</Link>
+      <Link href='/signup'>회원가입</Link>
+    </>
+  ),
+  employee: (
+    <>
+      <Link href='/my-profile'>내 프로필</Link>
+    </>
+  ),
+  employer: (
+    <>
+      <Link href='/my-shop'>내 가게</Link>
+    </>
+  ),
+};
 
 const Nav = () => {
+  const { role, isLogin, logout } = useAuth();
+  const navRole: UserRole = !isLogin ? 'guest' : role === 'employer' ? 'employer' : 'employee';
+
   return (
-    <nav className={cn('flex shrink-0 gap-4 text-body-m font-bold', 'desktop:gap-10')}>
-      <Link href={`/`}>로그인</Link>
-      <Link href={`/`}>회원가입</Link>
-      <button>
-        <Icon iconName='notificationOff' iconSize='rg' bigScreenSize='md' ariaLabel='알림' />
-      </button>
+    <nav className={cn('flex shrink-0 items-center gap-4 text-body-m font-bold', 'desktop:gap-10')}>
+      {NAV_CONFIG[navRole]}
+      {isLogin && (
+        <>
+          <button onClick={logout}>로그아웃</button>
+          <button>
+            <Icon iconName='notificationOff' iconSize='rg' bigScreenSize='md' ariaLabel='알림' />
+          </button>
+        </>
+      )}
     </nav>
   );
 };
