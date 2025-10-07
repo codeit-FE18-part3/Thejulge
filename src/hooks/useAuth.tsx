265f41119a78fac672ccae4b31aa46a6eb6ccbdf
celine <wisohyun0@gmail.com>
2025-10-07 19:16:08 +0900

    🔧 chore: 확장자 tsx-> ts 로 변경

--- src/hooks/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthContext } from '@/context/authProvider';
-import { useContext } from 'react';
-
-const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) throw new Error('useAuth는 AuthProvider 안에서 사용해야 합니다.');
-  return context;
-};
-export default useAuth;
