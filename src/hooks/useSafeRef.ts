024331360c811aac0c8a17587280f1844cda688c
celine <wisohyun0@gmail.com>
2025-10-08 02:33:25 +0900

    ✨ feat: ref 커스텀 훅 작성 #38

--- src/hooks/useSafeRef.ts
+++ src/hooks/useSafeRef.ts
@@ -2,7 +2,7 @@ import { useCallback, useRef } from 'react';
 
 // 안전하게 DOM 접근을 위한 hook
 // DOM이 없어질 수 있는 타이밍(언마운트, 조건부 렌더링 등)을 대비해서 존재 여부 확인
-export function useSafeRef<T extends HTMLElement>() {
+function useSafeRef<T extends HTMLElement>() {
   const ref = useRef<T | null>(null);
 
   const setRef = useCallback((node: T | null) => {
@@ -11,3 +11,4 @@ export function useSafeRef<T extends HTMLElement>() {
 
   return [setRef, ref] as const;
 }
+export default useSafeRef;
