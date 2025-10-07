a9689baf9674e9a0eefb74cfa358486db7ed7cb4
celine <wisohyun0@gmail.com>
2025-10-07 19:15:39 +0900

    ♻️ refactor: type 공통사용으로 개선 및 확장자 ts 로 변경   #38

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 
+// @example : useClickOutside(dropdownRef, setClose);
+type ClickOutsideHandler = (e: MouseEvent | TouchEvent) => void;
+
 const useClickOutside = <T extends HTMLElement>(
   ref: React.RefObject<T>,
-  handler: (event: MouseEvent | TouchEvent) => void
+  handler: ClickOutsideHandler
 ): void => {
   useEffect(() => {
-    const listener = (event: MouseEvent | TouchEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node)) return;
-      handler(event);
+    const listener: ClickOutsideHandler = e => {
+      if (!ref.current || ref.current.contains(e.target as Node)) return;
+      handler(e);
     };
 
     document.addEventListener('mousedown', listener);
