ad9de31e2e7b2f85f72bb6d97e3ec583c34f4965
celine <wisohyun0@gmail.com>
2025-10-08 04:02:17 +0900

    ✨ feat: 언마운트 시 null 값까지 체크하여 최신상태 반영 #38

--- src/hooks/useSafeRef.ts
+++ src/hooks/useSafeRef.ts
@@ -2,13 +2,13 @@ import { useCallback, useRef } from 'react';
 
 // 안전하게 DOM 접근을 위한 hook
 // DOM이 없어질 수 있는 타이밍(언마운트, 조건부 렌더링 등)을 대비해서 존재 여부 확인
-function useSafeRef<T extends HTMLElement>() {
+const useSafeRef = <T extends HTMLElement>() => {
   const ref = useRef<T | null>(null);
 
   const setRef = useCallback((node: T | null) => {
-    if (node) ref.current = node;
+    ref.current = node;
   }, []);
 
   return [setRef, ref] as const;
-}
+};
 export default useSafeRef;
