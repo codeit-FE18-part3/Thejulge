0d13ecc3c6a392cbae25b7982279c8f125244c69
gummmmmy0v0 <ppottatto524@gmail.com>
2025-10-08 19:21:01 +0900

    ♻️ refactor: currentMonth -> BaseCalendarProps 확장 #21

--- src/types/calendar.ts
+++ src/types/calendar.ts
@@ -7,15 +7,17 @@ export interface CalendarProps {
   onSelect?: (date: Date) => void;
 }
 
-export interface CalendarHeaderProps {
-  selectMode: SelectMode;
+export interface BaseCalendarProps {
   currentMonth: Date;
+}
+
+export interface CalendarHeaderProps extends BaseCalendarProps {
+  selectMode: SelectMode;
   onToggleMode: () => void;
   onChange: (offset: number) => void;
 }
 
-export interface DayViewProps {
-  currentMonth: Date;
+export interface DayViewProps extends BaseCalendarProps {
   currentDay: Date;
   onSelect: (date: Date) => void;
 }
@@ -24,8 +26,7 @@ export interface MonthViewProps {
   onSelect: (month: number) => void;
 }
 
-export interface YearViewProps {
-  currentMonth: Date;
+export interface YearViewProps extends BaseCalendarProps {
   onSelect: (year: number) => void;
 }
 
