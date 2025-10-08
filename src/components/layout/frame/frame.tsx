0c10ab5b056191460f48519d579aa2fb34135c89
BaeZzi813 <woehd8252@gmail.com>
2025-10-08 20:15:05 +0900

    ♻️ Refact: 반응형 div 태그로 묶어서 컨테이너 수정
    Refact: 반응형 div 태그로 묶어서 컨테이너 수정

--- src/components/layout/frame/frame.tsx
+++ src/components/layout/frame/frame.tsx
@@ -11,14 +11,16 @@ interface FrameProps {
 const Frame = ({ title, content, buttonText, href }: FrameProps) => {
   return (
     <>
-      <section className='flex flex-col gap-4 px-3 py-10 tablet:px-8 tablet:py-[60px] desktop:px-[237px] desktop:py-[60px]'>
-        <h1 className='text-heading-s font-bold tablet:text-heading-l'>{title}</h1>
-        <div className='flex flex-col items-center justify-center gap-4 rounded-xl border border-solid border-gray-200 px-6 py-[60px]'>
-          <h2 className='text-body-s font-normal tablet:text-body-l'>{content}</h2>
-          <div className='tablet:w-[346px]'>
-            <Button size='md' as={Link} href={href} full>
-              {buttonText}
-            </Button>
+      <section className='flex flex-col gap-4 px-3 py-10 tablet:px-8 tablet:py-[60px]'>
+        <div className='mx-auto w-full max-w-5xl'>
+          <h1 className='mb-4 text-heading-s font-bold tablet:text-heading-l'>{title}</h1>
+          <div className='flex flex-col items-center justify-center gap-4 rounded-xl border border-solid border-gray-200 px-6 py-[60px]'>
+            <h2 className='text-body-s font-normal tablet:text-body-l'>{content}</h2>
+            <div className='tablet:w-[346px]'>
+              <Button size='md' as={Link} href={href} full>
+                {buttonText}
+              </Button>
+            </div>
           </div>
         </div>
       </section>
