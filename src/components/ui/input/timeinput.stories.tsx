35f5a1c076eb4c3d15c26bf129b93067000a5005
gummmmmy0v0 <ppottatto524@gmail.com>
2025-10-09 21:17:13 +0900

    🐛fix: TimeInput 스토리북 불필요한 props 제거 #21

--- src/components/ui/input/timeinput.stories.tsx
+++ src/components/ui/input/timeinput.stories.tsx
@@ -10,16 +10,11 @@ export default meta;
 
 type Story = StoryObj<typeof TimeInput>;
 
-/* eslint-disable no-console */
-
 // 시간 입력 인풋 //
 export const Time: Story = {
   render: () => (
     <div>
-      <TimeInput
-        value={{ hours: 12, minutes: 30 }} // 초기값
-        onChange={time => console.log(time)}
-      />
+      <TimeInput />
     </div>
   ),
 };
