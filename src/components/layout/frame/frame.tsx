6624ae88592958347a2123ca6afe29e3f0811196
BaeZzi813 <woehd8252@gmail.com>
2025-10-09 11:59:22 +0900

    ♻️ Reafct: Container 적용 반응형 수정

--- src/components/layout/frame/frame.tsx
+++ src/components/layout/frame/frame.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@/components/layout/container';
 import Button from '@/components/ui/button/button';
 import Link from 'next/link';
 
@@ -11,19 +12,17 @@ interface FrameProps {
 const Frame = ({ title, content, buttonText, href }: FrameProps) => {
   return (
     <>
-      <section className='flex flex-col gap-4 px-3 py-10 tablet:px-8 tablet:py-[60px]'>
-        <div className='mx-auto w-full max-w-5xl'>
-          <h1 className='mb-4 text-heading-s font-bold tablet:text-heading-l'>{title}</h1>
-          <div className='flex flex-col items-center justify-center gap-4 rounded-xl border border-solid border-gray-200 px-6 py-[60px]'>
-            <h2 className='text-body-s font-normal tablet:text-body-l'>{content}</h2>
-            <div className='tablet:w-[346px]'>
-              <Button size='md' as={Link} href={href} full>
-                {buttonText}
-              </Button>
-            </div>
+      <Container as='section' className='flex flex-col gap-4 py-10 tablet:py-[60px]'>
+        <h1 className='text-heading-s font-bold tablet:text-heading-l'>{title}</h1>
+        <div className='flex flex-col items-center justify-center gap-4 rounded-xl border border-solid border-gray-200 px-6 py-[60px]'>
+          <h2 className='text-body-s font-normal tablet:text-body-l'>{content}</h2>
+          <div className='tablet:w-[346px]'>
+            <Button size='md' as={Link} href={href} full>
+              {buttonText}
+            </Button>
           </div>
         </div>
-      </section>
+      </Container>
     </>
   );
 };
