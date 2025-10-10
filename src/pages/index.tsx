7ae5a794ec4ccee54711b73274ecff90c3406ed0
celine <wisohyun0@gmail.com>
2025-10-10 16:51:09 +0900

    ✨ feat:  Card 컴포넌트 테스트용 뷰 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import PostWrapper from '@/components/ui/post/postWrapper';
+import NoticeWrapper from '@/components/ui/notice/mockData/noticeWrapper';
+import PostWrapper from '@/components/ui/post/mockData/postWrapper';
 
 export default function Home() {
   return (
     <>
+      <NoticeWrapper />
       <PostWrapper />
     </>
   );
