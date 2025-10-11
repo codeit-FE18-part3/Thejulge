d9c23e96378d1f8ed9269e08bb0c64b2f877f001
celine <wisohyun0@gmail.com>
2025-10-12 01:09:14 +0900

    🔧 chore: 불필요한 코드 삭제 #42

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import NoticeWrapper from '@/components/ui/card/notice/mockData/noticeWrapper';
 import PostWrapper from '@/components/ui/card/post/mockData/postWrapper';
-import Filter from '@/components/ui/filter/filter';
 
 export default function Home() {
   return (
     <>
-      <div className='relative flex justify-end'>
-        <button> 여기가 버튼임 </button>
-        <Filter className='right-0 top-0 min-[480px]:top-full' />
-      </div>
       <NoticeWrapper />
       <PostWrapper />
     </>
