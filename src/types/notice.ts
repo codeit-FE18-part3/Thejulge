fcd3678708549988e12938235157f31770b05942
celine <wisohyun0@gmail.com>
2025-10-14 02:29:35 +0900

    ✨ feat:  notice , api type추가 #59

--- src/types/notice.ts
+++ src/types/notice.ts
@@ -1,6 +1,7 @@
 /* -------------------- 공고 -------------------- */
 
-import { ShopSummary } from './shop';
+import { ApiResponse, Link, PaginatedResponse } from './api';
+import { Shop, ShopSummary } from './shop';
 
 // 공고 등록
 export interface NoticeBase {
@@ -24,3 +25,16 @@ export type NoticeCard = Notice &
     category: string;
     shopDescription: string;
   };
+
+export interface NoticeItemResponse {
+  item: Notice & {
+    shop: {
+      item: Shop;
+      href: string;
+    };
+  };
+  links: Link[];
+}
+
+export type NoticeListResponse = PaginatedResponse & ApiResponse<NoticeItemResponse>;
+export type NoticeVariant = 'notice' | 'shop';
