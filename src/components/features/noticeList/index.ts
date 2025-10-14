3a4ad8a7a407ff922c3cdeea0024f5a21ad50c2e
celine <wisohyun0@gmail.com>
2025-10-15 02:49:25 +0900

    🔧 chore:리팩토링하여 필요없는 파일 삭제  #51

--- src/components/features/noticeList/index.ts
+++ src/components/features/noticeList/index.ts
@@ -1,3 +1,1 @@
-export { default as AllNoticeList } from './allNoticeList';
-export { default as RecentNoticeList } from './recentNoticeList';
-export { default as RecommendedNoticeList } from './recommendedNoticeList';
+export { default as NoticeListSection } from './noticeListSection';
