5f443aaad0bb7d3eb53036335f81bcc287d435f4
celine <wisohyun0@gmail.com>
2025-10-15 02:50:43 +0900

    🔧 chore:리팩토링하여 필요없는 파일 삭제  #51

--- src/components/features/noticeList/recentNoticeList.tsx
+++ src/components/features/noticeList/recentNoticeList.tsx
@@ -1,11 +1,10 @@
-import { noticeListLayout } from '@/components/features/noticeList/noticeList.styles';
 import { Container } from '@/components/layout';
 
-// @TODO 최근에 본 공고 리스트 출력 
+// @TODO 최근에 본 공고 리스트 출력
 const RecentNoticeList = () => {
   return (
     <Container as='section' isPage>
-      <h2 className={noticeListLayout.title()}>최근에 본 공고</h2>
+      <h2 className=''>최근에 본 공고</h2>
     </Container>
   );
 };
