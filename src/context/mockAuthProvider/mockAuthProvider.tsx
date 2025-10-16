9c9c33f8245418fa79982aaeafa911ec66bf3a13
jeschun <jeschun54@gmail.com>
2025-10-16 14:31:45 +0900

    ✨ feat: mockdata 주석 처리

--- src/context/mockAuthProvider/mockAuthProvider.tsx
+++ src/context/mockAuthProvider/mockAuthProvider.tsx
@@ -1,25 +1,25 @@
-import { AuthContext } from '@/context/authProvider';
-import type { User, UserRole } from '@/types/user';
-import { ReactNode } from 'react';
+// import { AuthContext } from '@/context/authProvider';
+// import type { User, UserRole } from '@/types/user';
+// import { ReactNode } from 'react';
 
-interface MockAuthProviderProps {
-  user: User | null;
-  role?: UserRole; // guest | employer | employee
-  children: ReactNode;
-}
+// interface MockAuthProviderProps {
+//   user: User | null;
+//   role?: UserRole; // guest | employer | employee
+//   children: ReactNode;
+// }
 
-export const MockAuthProvider = ({ user, role, children }: MockAuthProviderProps) => {
-  const value = {
-    user,
-    isPending: false,
-    isLogin: role !== 'guest',
-    role: role ?? (user ? user.type : 'guest'),
-    login: async () => {},
-    logout: () => {},
-    signup: async () => {},
-    getUser: async () => {},
-    updateUser: async () => {},
-  };
+// export const MockAuthProvider = ({ user, role, children }: MockAuthProviderProps) => {
+//   const value = {
+//     user,
+//     isPending: false,
+//     isLogin: role !== 'guest',
+//     role: role ?? (user ? user.type : 'guest'),
+//     login: async () => {},
+//     logout: () => {},
+//     signup: async () => {},
+//     getUser: async () => {},
+//     updateUser: async () => {},
+//   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
+//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+// };
