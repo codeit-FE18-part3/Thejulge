9e812bd3c54f9b3c75904de5e9fd6d4380e0539f
celine <wisohyun0@gmail.com>
2025-10-18 02:18:48 +0900

    ✨ feat:  notice provider 제거 후 커스텀 훅으로 변경  #80

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,10 @@
-import { NoticeListSection } from '@/components/features';
-import { NoticeProvider } from '@/context/noticeProvider';
-import useAuth from '@/hooks/useAuth';
-import { useMemo } from 'react';
+import { CustomNotice, NoticeListSection } from '@/components/features';
 
 export default function Main() {
-  const { user } = useAuth();
-  const recommendedAddress = user?.address ?? '';
-  const initialFilters = useMemo(
-    () => ({ limit: 3, address: [recommendedAddress] }),
-    [recommendedAddress]
-  );
   return (
     <>
-      <NoticeProvider>
-        <div className='bg-red-100'>
-          <NoticeListSection title='맞춤 공고' showFilter={false} initialFilters={initialFilters} />
-        </div>
-      </NoticeProvider>
-      <NoticeProvider>
-        <NoticeListSection title='전체 공고' showFilter />;
-      </NoticeProvider>
+      <CustomNotice />
+      <NoticeListSection />;
     </>
   );
 }
