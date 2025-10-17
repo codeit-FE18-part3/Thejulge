d1c5a77fda187a6f7b97045260a33b582e5fb020
celine <wisohyun0@gmail.com>
2025-10-18 02:28:41 +0900

    ✨ feat:  가로 스크롤 컴포넌트 작성  #80

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,3 +2,4 @@ export { Container, Wrapper } from './container';
 export { Footer } from './footer';
 export { Frame } from './frame';
 export { Header } from './header';
+export { HorizontalScroll } from './horizontalScroll';
