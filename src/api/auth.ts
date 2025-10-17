bcb94d5598359db70968d38efb0c73170840e3a0
gummmmmy0v0 <ppottatto524@gmail.com>
2025-10-18 00:09:17 +0900

    🐛fix: 불필요한 코드 삭제 #79:

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -13,25 +13,3 @@ export async function apiSignup(body: UserRequest) {
   const { data } = await axios.post('/users', body);
   return data;
 }
-
-// 저는 이거 써야 로그인이 되어서...
-/*
-const API_BASE = process.env.NEXT_PUBLIC_API_URL;
-
-const apiClient = axios.create({
-  baseURL: API_BASE,
-});
-
-
-// 로그인: POST /token
-export async function apiLogin(body: LoginRequest) {
-  const { data } = await apiClient.post<LoginResponse>('/token', body);
-  return data; // data.item.token, data.item.user.item.id 사용
-}
-
-// 회원가입: POST /users (알바생이면 type: 'employee')
-export async function apiSignup(body: UserRequest) {
-  const { data } = await apiClient.post('/users', body);
-  return data;
-}
-  */
